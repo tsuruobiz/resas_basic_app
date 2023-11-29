27e7a8b4c0da4b76e561f1c6cb80a3d62b054668
tsuruooo <hideki.tsuruoka.fb@nttdocomo.com>
2023-11-29 15:12:36 +0900

    for文でループしてWidgetを配置した

--- lib/city/list_page.dart
+++ lib/city/list_page.dart
@@ -5,35 +5,37 @@ class CityListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cities = [
+      '札幌市',
+      '横浜市',
+      '川崎市',
+      '名古屋市',
+      '京都市',
+      '大阪市',
+      '堺市',
+      '神戸市',
+      '岡山市',
+      '広島市',
+      '北九州市',
+      '福岡市',
+      '熊本市',
+      '那覇市',
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
       body: ListView(children: [
-        ListTile(
-          title: const Text('札幌市'),
-          subtitle: const Text('政令指定都市'),
-          trailing: const Icon(Icons.navigate_next),
-          onTap: () {
-            // TODO: 詳細画面に遷移する
-          },
-        ),
-        ListTile(
-          title: const Text('横浜市'),
-          subtitle: const Text('政令指定都市'),
-          trailing: const Icon(Icons.navigate_next),
-          onTap: () {
-            // TODO: 詳細画面に遷移する
-          },
-        ),
-        ListTile(
-          title: const Text('川崎市'),
-          subtitle: const Text('政令指定都市'),
-          trailing: const Icon(Icons.navigate_next),
-          onTap: () {
-            // TODO: 詳細画面に遷移する
-          },
-        ),
+        for (final city in cities)
+          ListTile(
+            title: Text(city),
+            subtitle: const Text('政令指定都市'),
+            trailing: const Icon(Icons.navigate_next),
+            onTap: () {
+              // TODO: 詳細画面に遷移する
+            },
+          ),
       ]),
     );
   }

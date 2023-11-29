ee424a84c1251ddfde461894016222cc013df813
tsuruooo <hideki.tsuruoka.fb@nttdocomo.com>
2023-11-29 14:07:11 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# resas_basic_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/htsuruo/resas_basic_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.htsuruo.resas_basic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

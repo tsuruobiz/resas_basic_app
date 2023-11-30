5c38b2152322655ec669932e192426f36d67970a
tsuruooo <hideki.tsuruoka.fb@nttdocomo.com>
2023-11-30 15:37:43 +0900

    不要なベタ書きのリストを削除

--- lib/city/list_page.dart
+++ lib/city/list_page.dart
@@ -34,23 +34,6 @@ class _CityListPageState extends State<CityListPage> {
 
   @override
   Widget build(BuildContext context) {
-    final cities = [
-      '札幌市',
-      '横浜市',
-      '川崎市',
-      '名古屋市',
-      '京都市',
-      '大阪市',
-      '堺市',
-      '神戸市',
-      '岡山市',
-      '広島市',
-      '北九州市',
-      '福岡市',
-      '熊本市',
-      '那覇市',
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('市区町村一覧'),

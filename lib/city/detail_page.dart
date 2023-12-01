85692d73f6ab484a7889be162d089d44af1d8b81
tsuruooo <hideki.tsuruoka.fb@nttdocomo.com>
2023-12-01 13:21:54 +0900

    initStateで一人当たり地方税のエンドポイントを立たけるようにした

--- lib/city/detail_page.dart
+++ lib/city/detail_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../env.dart';
 
 class CityDetailPage extends StatefulWidget {
   const CityDetailPage({super.key, required this.city});
@@ -10,6 +13,29 @@ class CityDetailPage extends StatefulWidget {
 }
 
 class _CityDetailPageState extends State<CityDetailPage> {
+  late Future<String> _municipalitiesTaxesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    const host = 'opendata.resas-portal.go.jp';
+    const endpoint = '/api/v1/municipality/taxes/perYear';
+    final headers = {
+      'X-API-KEY': Env.resasApiKey,
+    };
+    final param = {
+      'prefCode': '14',
+      'cityCode': '14131',
+    };
+
+    _municipalitiesTaxesFuture = http
+        .get(
+          Uri.https(host, endpoint, param),
+          headers: headers,
+        )
+        .then((res) => res.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
